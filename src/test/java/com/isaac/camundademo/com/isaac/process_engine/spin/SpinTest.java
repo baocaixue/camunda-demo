e4c63302df21210b3ff1cca616cd118b56aa8a01
baocaixue <bao.isaac@gmail.com>
2019-10-28 11:09:25 +0800

    add Spin

---
+++ src/test/java/com/isaac/camundademo/com/isaac/process_engine/spin/SpinTest.java
@@ -0,0 +1,15 @@
+package com.isaac.camundademo.com.isaac.process_engine.spin;
+
+import org.camunda.spin.plugin.variable.SpinValues;
+import org.camunda.spin.plugin.variable.value.JsonValue;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SpinTest {
+    @Test
+    public void baseTest() {
+        String jsonStr = "{\"name\":\"jonny\",\"address\":{\"street\":\"12 High Street\",\"post code\":1234}}\n";
+        JsonValue jsonValue = SpinValues.jsonValue(jsonStr).create();
+        assertEquals("jonny", jsonValue.getValue().prop("name").stringValue());
+    }
+}
